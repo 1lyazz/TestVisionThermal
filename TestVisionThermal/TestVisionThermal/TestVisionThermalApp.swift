63312c5e341786488fc8b29e832cbeb24edae991
Ilya Zablotski <zablotski2020@gmail.com>
2025-03-31 14:45:21 +0300

    Setup

---
+++ TestVisionThermal/TestVisionThermal/TestVisionThermalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestVisionThermalApp.swift
+//  TestVisionThermal
+//
+//  Created by Ilya Zablotski on 31.03.25.
+//
+
+import SwiftUI
+
+@main
+struct TestVisionThermalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
